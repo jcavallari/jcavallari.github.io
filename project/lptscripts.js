ca3e7e7c35c4d0e08641ba65faa2962ade09590f
jcavallari <90336129+jcavallari@users.noreply.github.com>
2021-12-13 13:59:42 -0500

    Add files via upload

---
+++ project/lptscripts.js
@@ -0,0 +1,6 @@
+function openMail() {
+    window.open("mailto:podcast@limitedplaytime.com")
+  }
+  function openForm() {
+    window.open("lpt_signup_form.html", "newwindow", "width=550, height=550, left=150, top=200, toolbar=0, status=0")
+  }
